65a5f728bebdabbd3e63dce761d6c3518f1f43f9
Rajdeep <rajdeepmallick999@gmail.com>
2025-07-26 22:29:00 +0530

    🎉 modified changes for the light mode on hero section

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -3,14 +3,14 @@ import Link from 'next/link'
 import React from 'react'
 import '../globals.css'
 import { DiamondMinus,Linkedin } from 'lucide-react'
-import { ThreeDMarqueeDemoSecond } from '@/components/Hero'
+import { Hero } from '@/components/Hero'
 import { About } from '@/components/About'
 import { GitHubLogoIcon,InstagramLogoIcon } from '@radix-ui/react-icons'
 
 export default function Home() {
     return (
-        <div className='bg-black overflow-x-hidden'>
-            <ThreeDMarqueeDemoSecond />
+        <div className=' overflow-x-hidden'>
+            <Hero />
             <About />
             {/* <GlobeConnect/> */}
             <div className="flex flex-col justify-center items-center gap-8 my-24 mx-auto max-w-[100px]">
@@ -20,7 +20,7 @@ export default function Home() {
                     <Link href={'https://www.instagram.com/rajdeepmallick010/'}><InstagramLogoIcon className="h-6 w-6" /></Link>
                 </div>
                 <div className='flex gap-2 text-gray-500 text-sm mt-2'>
-                    <span>Insert2.0</span>{" "}<span>•</span>{" "}<span>2025</span>
+                    <span>Insert2.0</span>{" "}<span>•</span>{" "}<span>2025</span>•{" "}<Link href={'https://github.com/rajdeep010'}>rajdeep010</Link>
                 </div>
             </div>
         </div>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,20 +5,20 @@ import Link from "next/link";
 import { Spotlight } from "./ui/spotlight-new";
 import { BackgroundBeams } from "./ui/background-beams";
 
-export function ThreeDMarqueeDemoSecond() {
+export function Hero() {
     return (
         <div className="relative mx-auto flex h-screen w-screen  flex-col items-center justify-center overflow-hidden">
             <Spotlight/>
-            <div className='text-8xl z-20 font-sans tracking-wide text-white'>Insert</div>
+            <div className='text-8xl z-20 font-sans tracking-wide dark:text-white text-black'>Insert</div>
 
             {/* <div className='text-3xl z-20 font-sans text-white mt-[-0.5rem]'>2.0</div> */}
-            <p className="relative z-20 mx-auto max-w-[70%] lg:max-w-[40%] text-center font-sans text-md lg:text-2xl my-2 font-semibold text-white dark:text-gray-400">
+            <p className="relative z-20 mx-auto max-w-[70%] lg:max-w-[40%] text-center font-sans text-md lg:text-2xl my-2 font-semibold dark:text-white text-black">
                 Simple, More Powerful and made for Developers
             </p>
 
             <div className="relative z-20 flex mt-2 flex-wrap items-center justify-center gap-4">
                 <Link href='/sign-up'>
-                    <Button className="rounded-md text-sm font-medium border-2 border-white" variant="outline">
+                    <Button className="rounded-md text-sm font-medium border-2 dark:border-white border-black" variant="outline">
                         Get Started
                     </Button>
                 </Link>
