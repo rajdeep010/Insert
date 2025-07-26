0a9d576b0809e6c23e707a4bc671e4127a3967ad
Rajdeep <rajdeepmallick999@gmail.com>
2025-07-26 20:55:02 +0530

    🎉 changing to new look for the hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,43 +3,9 @@ import { ThreeDMarquee } from "@/components/ui/3d-marquee";
 import { Button } from "./ui/button";
 import Link from "next/link";
 import { Spotlight } from "./ui/spotlight-new";
+import { BackgroundBeams } from "./ui/background-beams";
 
 export function ThreeDMarqueeDemoSecond() {
-    const images = [
-        "https://res.cloudinary.com/dgxeg3sju/image/upload/v1753474491/31d46a44-bd00-4eac-85e3-a31b42e08f28.png",
-        "https://res.cloudinary.com/dgxeg3sju/image/upload/v1753474718/05969d69-43c7-4200-beca-173029a48450.png",
-        "https://res.cloudinary.com/dgxeg3sju/image/upload/v1753474830/4440e28b-4269-42b1-9536-179d06eb8f80.png",
-        "https://res.cloudinary.com/dgxeg3sju/image/upload/v1753474932/1c94ae34-4c35-40d8-b699-cb4cce5c20b2.png",
-        "https://res.cloudinary.com/dgxeg3sju/image/upload/v1753475010/52b47260-89ec-4576-b33d-05bd1dfd8328.png",
-        "https://res.cloudinary.com/dgxeg3sju/image/upload/v1753474830/4440e28b-4269-42b1-9536-179d06eb8f80.png",
-        "https://res.cloudinary.com/dgxeg3sju/image/upload/v1753475010/52b47260-89ec-4576-b33d-05bd1dfd8328.png",
-        "https://res.cloudinary.com/dgxeg3sju/image/upload/v1753474830/4440e28b-4269-42b1-9536-179d06eb8f80.png",
-        "https://res.cloudinary.com/dgxeg3sju/image/upload/v1753475519/c76939bd-fc4d-426b-b247-0c1db4966016.png",
-        "https://res.cloudinary.com/dgxeg3sju/image/upload/v1753475010/52b47260-89ec-4576-b33d-05bd1dfd8328.png",
-        "https://res.cloudinary.com/dgxeg3sju/image/upload/v1753474830/4440e28b-4269-42b1-9536-179d06eb8f80.png",
-        "https://res.cloudinary.com/dgxeg3sju/image/upload/v1753475519/c76939bd-fc4d-426b-b247-0c1db4966016.png",
-        "https://res.cloudinary.com/dgxeg3sju/image/upload/v1753474830/4440e28b-4269-42b1-9536-179d06eb8f80.png",
-        "https://res.cloudinary.com/dgxeg3sju/image/upload/v1753475010/52b47260-89ec-4576-b33d-05bd1dfd8328.png",
-        "https://res.cloudinary.com/dgxeg3sju/image/upload/v1753475519/c76939bd-fc4d-426b-b247-0c1db4966016.png",
-        "https://res.cloudinary.com/dgxeg3sju/image/upload/v1753475010/52b47260-89ec-4576-b33d-05bd1dfd8328.png",
-        "https://res.cloudinary.com/dgxeg3sju/image/upload/v1753474718/05969d69-43c7-4200-beca-173029a48450.png",
-        "https://res.cloudinary.com/dgxeg3sju/image/upload/v1753475519/c76939bd-fc4d-426b-b247-0c1db4966016.png",
-        "https://res.cloudinary.com/dgxeg3sju/image/upload/v1753474830/4440e28b-4269-42b1-9536-179d06eb8f80.png",
-        "https://res.cloudinary.com/dgxeg3sju/image/upload/v1753474932/1c94ae34-4c35-40d8-b699-cb4cce5c20b2.png",
-        "https://res.cloudinary.com/dgxeg3sju/image/upload/v1753475010/52b47260-89ec-4576-b33d-05bd1dfd8328.png",
-        "https://res.cloudinary.com/dgxeg3sju/image/upload/v1753474830/4440e28b-4269-42b1-9536-179d06eb8f80.png",
-        "https://res.cloudinary.com/dgxeg3sju/image/upload/v1753474830/4440e28b-4269-42b1-9536-179d06eb8f80.png",
-        "https://res.cloudinary.com/dgxeg3sju/image/upload/v1753474718/05969d69-43c7-4200-beca-173029a48450.png",
-        "https://res.cloudinary.com/dgxeg3sju/image/upload/v1753474830/4440e28b-4269-42b1-9536-179d06eb8f80.png",
-        "https://res.cloudinary.com/dgxeg3sju/image/upload/v1753474932/1c94ae34-4c35-40d8-b699-cb4cce5c20b2.png",
-        "https://res.cloudinary.com/dgxeg3sju/image/upload/v1753475010/52b47260-89ec-4576-b33d-05bd1dfd8328.png",
-        "https://res.cloudinary.com/dgxeg3sju/image/upload/v1753474830/4440e28b-4269-42b1-9536-179d06eb8f80.png",
-        "https://res.cloudinary.com/dgxeg3sju/image/upload/v1753474718/05969d69-43c7-4200-beca-173029a48450.png",
-        "https://res.cloudinary.com/dgxeg3sju/image/upload/v1753474830/4440e28b-4269-42b1-9536-179d06eb8f80.png",
-        "https://res.cloudinary.com/dgxeg3sju/image/upload/v1753474932/1c94ae34-4c35-40d8-b699-cb4cce5c20b2.png",
-        "https://res.cloudinary.com/dgxeg3sju/image/upload/v1753475010/52b47260-89ec-4576-b33d-05bd1dfd8328.png",
-        "https://res.cloudinary.com/dgxeg3sju/image/upload/v1753474830/4440e28b-4269-42b1-9536-179d06eb8f80.png",
-    ];
     return (
         <div className="relative mx-auto flex h-screen w-screen  flex-col items-center justify-center overflow-hidden">
             <Spotlight/>
@@ -57,13 +23,7 @@ export function ThreeDMarqueeDemoSecond() {
                     </Button>
                 </Link>
             </div>
-
-            {/* overlay */}
-            <div className="absolute inset-0 z-10 h-full w-full bg-black/80 dark:bg-black/40" />
-            <ThreeDMarquee
-                className="pointer-events-none absolute inset-0 h-screen w-screen"
-                images={images}
-            />
+            <BackgroundBeams />
         </div>
     );
 }
