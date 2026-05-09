15d1214d829972890785ea0ee6854f828a378727
rajdeepmallick <rajdeepmallick999@gmail.com>
2026-05-09 07:03:55 +0530

    changes for imports

--- src/app/(app)/posts/blog/[blogUrl]/page.tsx
+++ src/app/(app)/posts/blog/[blogUrl]/page.tsx
@@ -14,56 +14,20 @@ import { Underline } from "@tiptap/extension-underline";
 import { Link } from "@/components/tiptap-extension/link-extension";
 import { Selection } from "@/components/tiptap-extension/selection-extension";
 import { TrailingNode } from "@/components/tiptap-extension/trailing-node-extension";
-import { Button } from "@/components/tiptap-ui-primitive/button";
-import { Spacer } from "@/components/tiptap-ui-primitive/spacer";
-import {
-    Toolbar,
-    ToolbarGroup,
-    ToolbarSeparator,
-} from "@/components/tiptap-ui-primitive/toolbar";
 import { ImageUploadNode } from "@/components/tiptap-node/image-upload-node/image-upload-node-extension";
 import "@/components/tiptap-node/code-block-node/code-block-node.scss";
 import "@/components/tiptap-node/list-node/list-node.scss";
 import "@/components/tiptap-node/image-node/image-node.scss";
 import "@/components/tiptap-node/paragraph-node/paragraph-node.scss";
-import { HeadingDropdownMenu } from "@/components/tiptap-ui/heading-dropdown-menu";
-import { ImageUploadButton } from "@/components/tiptap-ui/image-upload-button";
-import { ListDropdownMenu } from "@/components/tiptap-ui/list-dropdown-menu";
-import { BlockQuoteButton } from "@/components/tiptap-ui/blockquote-button";
-import { CodeBlockButton } from "@/components/tiptap-ui/code-block-button";
-import {
-    ColorHighlightPopover,
-    ColorHighlightPopoverContent,
-    ColorHighlightPopoverButton,
-} from "@/components/tiptap-ui/color-highlight-popover";
-import {
-    LinkPopover,
-    LinkContent,
-    LinkButton,
-} from "@/components/tiptap-ui/link-popover";
-import { MarkButton } from "@/components/tiptap-ui/mark-button";
-import { TextAlignButton } from "@/components/tiptap-ui/text-align-button";
-import { UndoRedoButton } from "@/components/tiptap-ui/undo-redo-button";
-import { ArrowLeftIcon } from "@/components/tiptap-icons/arrow-left-icon";
-import { HighlighterIcon } from "@/components/tiptap-icons/highlighter-icon";
-import { LinkIcon } from "@/components/tiptap-icons/link-icon";
-import { useMobile } from "@/hooks/use-mobile";
-import { useWindowSize } from "@/hooks/use-window-size";
-import { useCursorVisibility } from "@/hooks/use-cursor-visibility";
-import { Button as Btn } from "@/components/ui/button";
 import { handleImageUpload, MAX_FILE_SIZE } from "@/lib/tiptap-utils";
 import "@/components/tiptap-templates/simple/simple-editor.scss";
-import { useBlog } from "@/features/blog/context/BlogProvider";
-import { useSession } from "next-auth/react";
-import { useParams, useRouter } from "next/navigation";
-import { Skeleton } from "@/components/ui/skeleton";
+import { useRouter } from "next/navigation";
 import { Loader2 } from "lucide-react";
 import { useToast } from "@/components/ui/use-toast";
 import axios from "axios";
 import InsertNavbar from "@/components/InsertNavbar";
 import InsertHoverCard from "@/components/InsertHoverCard";
 import { getLastModifiedText } from "@/helpers/last-modified";
-import BlogWriteSidebar from "@/components/BlogWriteSidebar";
 import { CalendarDays, Clock3, Info, PenSquare } from "lucide-react";
 
 
